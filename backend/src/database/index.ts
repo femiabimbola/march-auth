60016bf31748f2e295b9a71d7e4b847e645f3ddd
femi <femiabimbola6@gmail.com>
2026-03-08 15:05:16 -0700

    setting up drizzle config

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 // Local Import
-import { config } from '../lib/config/app.config.js';
+import { config } from '../lib/config/app.config';
 
 export const db = drizzle({ 
   connection: { connectionString: config.DATABASE_URL, ssl: true  }
